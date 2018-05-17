e252bae7c1e274d85d8decc028bc6f165fd9ef59
dani7cl <daniel19.cl@gmail.com>
2018-05-17 21:34:14 +0200

    login fix

--- src/controllers/authentication.controller.ts
+++ src/controllers/authentication.controller.ts
@@ -4,6 +4,7 @@ class AuthenticationController {
 	private _authenticationService: AuthenticationService = new AuthenticationService()
 
 	login = async (req, res) => {
+		if(!req.body.email) return res.status(400).send('Bad Request')
 		if (req.session.email) return res.status(400).send('Already logged in')
 		if (!await this._authenticationService.setSessionData(req)) {
 			res.status(401).send('Unauthorized')
