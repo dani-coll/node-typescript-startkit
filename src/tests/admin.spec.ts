1794968cc46020c3c5e881a244be63ab9142f142
dani7cl <daniel19.cl@gmail.com>
2018-05-17 21:15:17 +0200

    tests

--- src/tests/admin.spec.ts
+++ src/tests/admin.spec.ts
@@ -5,15 +5,6 @@ import app from '../app'
 
 const adminSession = session(app)
 
-describe('Admin Tests \n 1 - getting a user', () => {
-	it('should return unauthorized', () =>
-		adminSession
-			.get('/api/user?id=a0ece5db-cd14-4f21-812f-966633e7be86')
-			.expect('Content-Type', 'text/html; charset=utf-8')
-			.expect(401)
-	)
-})
-
 describe('2 - Login', () => {
 	it('should return success', () =>
 		adminSession
@@ -38,63 +29,4 @@ describe('3 - Login', () => {
 			.expect(400)
 			.expect('Content-Type', 'text/html; charset=utf-8')
 	)
-})
-
-const userId = 'a0ece5db-cd14-4f21-812f-966633e7be86'
-describe('4 - getting a user', () => {
-	it('should return the user', (done) => {
-		adminSession
-			.get(`/api/user?id=${userId}`)
-			.expect(200)
-			.end((err, res) => {
-				if (err) return done(err)
-				if (res.body.id === userId) done()
-			  })
-	})
-})
-
-describe('5 - getting a user', () => {
-	it('should return not found', () =>
-		adminSession
-			.get('/api/user?id=sadsads')
-			.expect('Content-Type', 'text/html; charset=utf-8')
-			.expect(404)
-	)
-})
-
-const username = 'Whitley'
-describe('6 - getting a user', () => {
-	it('should return the user', (done) => {
-		adminSession
-			.get(`/api/user?name=${username}`)
-			.expect(200)
-			.end((err, res) => {
-				if (err) return done(err)
-				if (res.body.name === username) done()
-			  })
-	})
-})
-
-describe('7 - getting the user policies', () => {
-	it('should return the user policies', (done) => {
-		adminSession
-			.get('/api/user/Britney/policies')
-			.expect(200)
-			.end((err, res) => {
-				if (err) return done(err)
-				if (Array.isArray(res.body)) done()
-			})
-	})
-})
-
-describe('8 - User linked to the policy', () => {
-	it('should return the user linked to the policy', (done) => {
-		adminSession
-			.get('/api/policy/56b415d6-53ee-4481-994f-4bffa47b5239/user')
-			.expect(200)
-			.end((err, res) => {
-				if (err) return done(err)
-				if (res.body && res.body.id && res.body.role) done()
-			})
-	})
 })
